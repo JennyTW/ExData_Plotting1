ee38b36e07923d018508422c2f941887f1ffe886
JennyTW <foo_rong@yahoo.com.tw>
2015-06-08 07:09:44 +0800

    Plot3andPlot4
    Plot3andPlot4

---
+++ plot3.R
@@ -0,0 +1,26 @@
+##Data1: load all data
+TopData1<-read.table("household_power_consumption.txt", nrows=50, header=TRUE, sep=";" )
+DataClass<-sapply(TopData1, class)
+Data1<-read.table("household_power_consumption.txt", header=TRUE,sep=";",colClass=DataClass, na.strings="?")
+
+##Data2: extract data from 2007-02-01 and 2007-02-02.
+Data1$Date<-as.Date(Data1$Date, format = "%d/%m/%Y")
+Date2007<-as.Date(c("2007-02-01","2007-02-02"))
+Data2<-Data1[Data1$Date %in% Date2007, ]
+
+##Data3: conbine Date and Time
+Date1<-as.character(Data2$Date)
+Time1<-as.character(Data2$Time)
+DT<-paste(Date1,Time1)
+DT<-strptime(DT, format="%Y-%m-%d %H:%M:%S")
+Data3<-cbind(Data2,DT)
+
+
+##Plot3
+
+plot(Data3$DT,Data3$Sub_metering_1, xlab="",type="l",col="black",ylab="Energy sub metering")
+lines(Data3$DT,Data3$Sub_metering_2, xlab="",type="l",col="red")
+lines(Data3$DT,Data3$Sub_metering_3, xlab="",type="l",col="blue")
+legend("topright",lty=c(1,1,1),col=c("black","red","blue"),legend=c("Sub_meeting_1","Sub_meeting_2","Sub_meeting_3"))
+dev.copy(png,file="plot3.png",width = 480, height = 480, units = "px")
+dev.off()
